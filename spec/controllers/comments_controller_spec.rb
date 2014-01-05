6e65535f2c71b14cde64dbeecf55a0a3da880128
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-01-04 18:54:05 -0800

    testing on comments complete

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe CommentsController do
+  describe 'GET #create' do
+    it 'does create new comment' do
+      post = Post.create(title: 'The title', text: 'The content')
 
+      get :create, post_id: post.id, comment: {
+                                              body: 'The content',
+                                              commenter: 'name' }
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'does delete comments' do
+      post = Post.create( title: 'The title', text: 'The Content')
+      comment = Comment.create( body: 'The content', commenter: 'name', post_id: post.id )
+
+      delete :destroy, post_id: post.id, id: comment.id
+
+      expect(response).to redirect_to "/posts/#{post.id}"
+    end
+  end
 end
