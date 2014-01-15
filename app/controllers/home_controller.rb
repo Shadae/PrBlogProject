0bc202c3e028bc1524e0f276aa761a74a168f51a
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-01-14 16:23:28 -0800

    Add functionality to visitor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 # Home Controller
 # Author: Dae
 class HomeController < ApplicationController
+  before_action :update_visit_count
+
   def index
+  end
+
+  def update_visit_count
+    current_visitor.update(page_visits: current_visitor.page_visits + 1, last_visit: Time.now)
   end
 end
