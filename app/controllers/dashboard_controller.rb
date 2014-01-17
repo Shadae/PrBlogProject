928ab6f64b3f3b139cc06e88cdb18085c516ae5c
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-01-17 14:38:46 -0800

    Dashboard Cookie update

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
+  before_action :update_visit_count
+  
   def index
+  end
+
+  def update_visit_count
+    current_visitor.update(page_visits: current_visitor.page_visits + 1, last_visit: Time.now)
   end
 end
